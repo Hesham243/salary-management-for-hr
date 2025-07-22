f4abac26aefcdb764fde52f2f2384cc8cad6f8a3
Hesham243 <heshamahmed243@gmail.com>
2025-07-22 09:44:47 +0300

    finish delete + update on payroll model

--- controllers/payrolls.controller.js
+++ controllers/payrolls.controller.js
@@ -7,12 +7,7 @@ router.get('/:employeeId/new', (req, res) => {
   res.render('payrolls/new.ejs', { employeeId: req.params.employeeId })
 })
 
-// router.post('/:employeeId', isSignedIn, async (req, res) => {
-//   req.body.user = req.session.user._id
-//   req.body.employee = req.params.employeeId
-//   const createPayroll = await Payroll.create(req.body)
-//   res.redirect(`/payrolls/${req.params.employeeId}/${createPayroll._id}`)
-// })
+
 router.post('/:employeeId', isSignedIn, async (req, res) => {
   req.body.user = req.session.user._id
   req.body.employee = req.params.employeeId
@@ -20,10 +15,6 @@ router.post('/:employeeId', isSignedIn, async (req, res) => {
   res.redirect(`/payrolls/${req.params.employeeId}`)
 })
 
-// router.get('/:employeeId/:payrollId', async (req, res) => {
-//   const foundPayroll = await Payroll.findById(req.params.payrollId).populate('user').populate('employee')
-//   res.render('payrolls/show.ejs', {foundPayroll: foundPayroll})
-// })
 
 router.get('/:employeeId', async (req, res) => {
   console.log(req.params.employeeId)
@@ -34,6 +25,38 @@ router.get('/:employeeId', async (req, res) => {
 })
 
 
+router.delete('/:employeeId/:payrollId', isSignedIn, async (req, res) => {
+  const foundPayroll = await Payroll.findById(req.params.payrollId).populate('user').populate('employee')
+  if (foundPayroll.user._id.equals(req.session.user._id)){
+    await foundPayroll.deleteOne()
+    console.log('This is the deleted payroll from db:', foundPayroll)
+    return res.redirect(`/payrolls/${req.params.employeeId}`)
+  }else {
+    res.send('Not Authorized')
+  }
+})
+
+
+router.get('/:employeeId/:payrollId/edit',isSignedIn , async (req, res) => {
+  const foundPayroll = await Payroll.findById(req.params.payrollId).populate('user').populate('employee')
+  if (foundPayroll.user._id.equals(req.session.user._id)){
+    return res.render('payrolls/edit.ejs', {foundPayroll: foundPayroll})
+  } else {
+    res.send('Not Authorized')
+  }
+})
+
+
+router.put('/:employeeId/:payrollId', isSignedIn, async (req, res) => {
+  const foundPayroll = await Payroll.findById(req.params.payrollId).populate('user').populate('employee')
+  if (foundPayroll.user._id.equals(req.session.user._id)){
+    const updatedPayroll = await Payroll.findByIdAndUpdate(req.params.payrollId, req.body, { new: true })
+    console.log('This is the updated payroll from db:', updatedPayroll)
+    return res.redirect(`/payrolls/${foundPayroll.employee._id}`)
+  } else {
+    return res.send('Not Authorized')
+  }
+})
 
 
 module.exports = router
